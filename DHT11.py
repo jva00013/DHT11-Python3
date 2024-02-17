fe6c110337c0bf2366bcab3f85497e6ed6fab06b
José Valle Aguilera <jva00013@red.ujaen.es>
2024-02-17 11:54:20 +0100

    Update DHT11.py

--- DHT11.py
+++ DHT11.py
@@ -11,11 +11,11 @@ import Freenove_DHT as DHT
 DHTPin = 11     #define the pin of DHT11
 
 def loop():
-    dht = DHT.DHT(DHTPin)   #create a DHT class object
-    while(True):
+	dht = DHT.DHT(DHTPin)   #create a DHT class object
+	while(True):
         chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-            if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
+		if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+        	print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
         time.sleep(2)       
         
 if __name__ == '__main__':
